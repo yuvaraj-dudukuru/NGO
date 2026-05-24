9b2759ca7ee60497111c7f8fc0df45bbdb5f770a
7227Yuvaraj <dudukuruyuvaraj55@gmail.com>
2026-05-24 21:29:51 +0530

    Final local project

--- web+app/day_4/index.js
+++ web+app/day_4/index.js
@@ -1,13 +1,13 @@
 let password = document.getElementById("password");
 let power = document.getElementById("power-point");
 
-password.onabort = function() {
+password.oninput = function() {
     let point = 0;
     let value = password.value;
-    let widthpower = ["1%", "25%", "50%", "75%", "100%"];
+    let widthpower = ["0%", "25%", "50%", "75%", "100%"];
     let color = ["red", "orange", "yellow", "lightgreen", "green"];
 
-    if (value.lenght >= 6){
+    if (value.length >= 6){
         let arrayTest = [/[a-z]/, /[A-Z]/, /[0-9]/, /[^a-zA-Z0-9]/];
         arrayTest.forEach((item) => {
             if (item.test(value)){
@@ -15,8 +15,8 @@ password.onabort = function() {
             }
         });
     }
-    power.style.width = widthpower[point - 1] || "0";
-    power.style.backgroundColor = color[point - 1] || "red";
+    power.style.width = widthpower[point];
+    power.style.backgroundColor = color[point];
 }
 
 
