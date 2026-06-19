8303a1e29982898352c87860bfbcf88ff7fc4172
dudukuru yuvaraj <dudukuruyuvaraj55@gmail.com>
2026-06-19 18:58:18 +0530

    add day-12 ml, ml_02, web+app day-14, web+app_02 content and root .gitignore

---
+++ web+app_02/day-5/do.js
@@ -0,0 +1,9 @@
+// Traditional Version
+function add(a, b) {
+    return a + b;
+}
+console.log(add(5, 3));
+
+// Arrow Version (Renamed to clear the collision)
+const addArrow = (a, b) => a + b;
+console.log(addArrow(5, 3));
